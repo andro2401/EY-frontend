bde982ba8a1ff33746ace5c8f836662371c22942
Andro <androistuk@gmail.com>
2023-07-16 12:52:48 +0200

    added swiper section and made hero and about sections responsive for mobile

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 2,
+    centeredSlides: true,
+
+    navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+    },
+  });
+});
